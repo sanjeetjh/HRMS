5452c9f375593a6b2a3ad905647d40fb476069de
Sanjeet Jha <sanjeet.jha@gritinnovation.com>
2017-02-12 21:14:50 +0530

    First commit

---
+++ src/main/java/in/sanjeetjh/hrms/HrmsApplication.java
@@ -0,0 +1,12 @@
+package in.sanjeetjh.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsApplication.class, args);
+	}
+}
